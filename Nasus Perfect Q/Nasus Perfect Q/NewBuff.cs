9763339e93e71e858dfe046e06c9d8634a42d2b8
Reddzy <leagueofreddzy@gmail.com>
2014-12-02 14:18:05 +0100

    Nasus Perfect Q
    Nasus Perfect Q

---
+++ Nasus Perfect Q/Nasus Perfect Q/NewBuff.cs
@@ -0,0 +1,14 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Nasus_Perfect_Q
+{
+    internal class NewBuff
+    {
+        public string DisplayName { set; get; }
+        public string Name { set; get; }
+    }
+}
